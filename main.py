077997a7cafb127cbb1f74499cd984ff05777afc
Praveena Sivakumar <82393502+Praveenajw@users.noreply.github.com>
2022-04-03 18:39:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+import tkinter as tk
+import csv
+import cv2
+import os
+import numpy as np
+from PIL import Image
+import pandas as pd
+
+
+window = tk.Tk()
+window.title("FACE RECOGNITION SYSTEM")
+window.geometry('800x500')
+
+dialog_title = 'QUIT'
+dialog_text = "are you sure?"
+window.configure(background='green')
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+
+
+def clear():
+    std_name.delete(0, 'end')
+    res = ""
+    label4.configure(text=res)
+
+
+def clear2():
+    std_number.delete(0, 'end')
+    res = ""
+    label4.configure(text=res)
+
+
+def takeImage():
+    name = (std_name.get())
+    Id = (std_number.get())
+    if name.isalpha():
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+
+        while True:
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.1, 3)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                sampleNum = sampleNum + 1
+                # store each student picture with its name and id
+                cv2.imwrite("TrainingImages\ " + name + "." + Id + '.' + str(sampleNum) + ".jpg",
+                            gray[y:y + h, x:x + h])
+                cv2.imshow('FACE RECOGNIZER', img)
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            # stop the camera when the number of picture exceed 50 pictures for each student
+            if sampleNum > 50:
+                break
+
+        cam.release()
+        cv2.destroyAllWindows()
+        # print the student name and id after a successful face capturing
+        res = 'Visiter details saved with: \n Phone number : ' + Id + ' and  Full Name: ' + name
+
+        row = [Id, name]
+
+        with open('visiters-details.csv', 'a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        label4.configure(text=res)
+    else:
+
+        if name.isalpha():
+            res = "Enter correct Matric Number"
+            label4.configure(text=res)
+
+
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faces = []
+    Ids = []
+    for imagePath in imagePaths:
+        pilImage = Image.open(imagePath).convert('L')
+        imageNp = np.array(pilImage, 'uint8')
+        Id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces.append(imageNp)
+        Ids.append(Id)
+    return faces, Ids
+
+label1 = tk.Label(window, background="green", fg="black", text="Name :", width=10, height=1,
+                  font=('Helvetica', 16))
+label1.place(x=83, y=40)
+std_name = tk.Entry(window, background="yellow", fg="black", width=25, font=('Helvetica', 14))
+std_name.place(x=280, y=41)
+label2 = tk.Label(window, background="green", fg="black", text="Phone Number :", width=14, height=1,
+                  font=('Helvetica', 16))
+label2.place(x=100, y=90)
+std_number = tk.Entry(window, background="yellow", fg="black", width=25, font=('Helvetica', 14))
+std_number.place(x=280, y=91)
+
+clearBtn1 = tk.Button(window, background="red", command=clear, fg="white", text="CLEAR", width=8, height=1,
+                      activebackground="red", font=('Helvetica', 10))
+clearBtn1.place(x=580, y=42)
+clearBtn2 = tk.Button(window, background="red", command=clear2, fg="white", text="CLEAR", width=8,
+                      activebackground="red", height=1, font=('Helvetica', 10))
+clearBtn2.place(x=580, y=92)
+
+label3 = tk.Label(window, background="green", fg="red", text="Notification", width=10, height=1,
+                  font=('Helvetica', 20, 'underline'))
+label3.place(x=320, y=155)
+label4 = tk.Label(window, background="yellow", fg="black", width=55, height=4, font=('Helvetica', 14, 'italic'))
+label4.place(x=95, y=205)
+
+takeImageBtn = tk.Button(window, command=takeImage, background="yellow", fg="black", text="CAPTURE IMAGE",
+                         activebackground="red",
+                         width=60, height=3, font=('Helvetica', 12))
+takeImageBtn.place(x=130, y=360)
+
+
+window.mainloop()
